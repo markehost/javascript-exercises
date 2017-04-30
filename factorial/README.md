a34cf4a00a93ab9aac29ec6d5e75f4c4172349fd
markehost <mark.e.host@gmail.com>
2017-04-30 12:50:15 -0400

    factorial question

--- factorial/README.md
+++ factorial/README.md
@@ -1,9 +1,26 @@
-Factorial is a number that can be represented in a function, and often asked in interviews.
+In mathematics `factorial` [is the product of all positive integers less than or equal to n](https://en.wikipedia.org/wiki/Factorial) denoted as `n!`, given an integer `n`. The factorial function is a representation of `n!`, and this is what we will be creating.
 
-`factorial` blah blah: 
+Below is a start on the `index.js` file we will create in the `solutions` branch.
+
+
 
 ```js
-// javascript code example here
+
+let factorial = {
+
+	// recursive
+	recursive: function recursive(num) {
+
+	},
+
+	// iterative
+	iterative: function iterative(num) {
+
+	}
+
+}
+
+module.exports = factorial
 
 ```
 
--- factorial/test.js
+++ factorial/test.js
@@ -1,2 +1,30 @@
 var assert = require('assert');
 var factorial = require('./');
+
+
+describe('factorial', function() {
+
+	it('will return the factorial of an integer - using recursion', function() {
+		assert.equal( factorial.recursive(9), 362880 );
+	});
+
+	it('will return the factorial of an integer - using iteration', function() {
+		assert.equal( factorial.iterative(8), 40320 );
+	});
+
+	it('will return false for non-integers - strings', function() {
+		assert.equal( factorial.recursive('abc'), false );
+	});
+
+	it('will return false for non-integers - null', function() {
+		assert.equal( factorial.recursive(null), false );
+	});
+
+
+	// TODO: need more test cases
+
+	// it('will return false for non-integers - negative number', function() {
+	// 	assert.equal( factorial.recursive(-1), false );
+	// });
+
+});
