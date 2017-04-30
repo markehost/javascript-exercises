f577d439890eea743abe9bda770b6ef6976005c6
markehost <mark.e.host@gmail.com>
2017-04-30 12:31:16 -0400

    isNumeric master

--- isNumeric/index.js
+++
@@ -1,9 +0,0 @@
-
-function isNumeric(num) {
-	// console.log("number is ---> ", num )
-	// console.log("number? ", !isNaN( parseFloat(num) ) && isFinite(num) )
-
-	return !isNaN( parseFloat(num) ) && isFinite(num)
-}
-
-module.exports = isNumeric
