d54623afe3e039f8ee292eb26349001030b3b3d6
markehost <mark.e.host@gmail.com>
2017-05-02 00:09:52 -0400

    remove comments

--- isNumeric/index.js
+++ isNumeric/index.js
@@ -1,8 +1,5 @@
 
 function isNumeric(num) {
-	// console.log("number is ---> ", num )
-	// console.log("number? ", !isNaN( parseFloat(num) ) && isFinite(num) )
-
 	return !isNaN( parseFloat(num) ) && isFinite(num)
 }
 
