97588a59f2530d21ffbd2590a68ff1e44a95622f
markehost <mark.e.host@gmail.com>
2017-05-02 20:41:18 -0400

    binary to integer solution

---
+++ binary_string_to_integer/index.js
@@ -0,0 +1,10 @@
+
+function binaryStringToInteger(b) {
+
+	// console.log("number -->", b);
+	// console.log("is integer??? -->", ( b^0 ) === b);
+
+	return parseInt( b, 2 )
+}
+
+module.exports = binaryStringToInteger
